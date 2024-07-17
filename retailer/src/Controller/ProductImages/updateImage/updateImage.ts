7658e6d3057e1dc53a3f15653f1fd1c454ac220b
shivammishra00 <shivam887823950@gmail.com>
2024-07-17 13:05:36 +0530

    problem solved updateImage.ts

--- retailer/src/Controller/ProductImages/updateImage/updateImage.ts
+++ retailer/src/Controller/ProductImages/updateImage/updateImage.ts
@@ -2,9 +2,10 @@ import express, {Request, Response} from 'express';
 
 const connection = require('../../../Model/dbConnected');
 
-
-const updateImage = (req:Request | any, res:Response) => {
+const updateImage = async (req:Request | any, res:Response) => {
     const imageid:string = req.params.imageid;
+
+    // here apply validation //
     if(!imageid){
         return res.json({Error: "imageid is required"})
     }
@@ -12,21 +13,29 @@ const updateImage = (req:Request | any, res:Response) => {
         return res.json({Error: "image is required"})
     }
     // console.log(req.file)
+
+    //////==========  validation end ===========////////////
+
     const data:string[] = [
         req.file.location,
         req.body.description,
         req.body.color,
     ]
 
+    // here also apply validation //
     const fields:string[] = ['image', 'description', 'color'];
     for(let i=0; i<fields.length; i++){
         if(!data[i]){
             return res.json({Error: `${fields[i]} is required`})
         }
     }
+
+    // ========== validation end ===============//
+
     console.log(data)
+
     const sqlQuery = ` UPDATE tbl_retailer_product_images SET image=?, description=?, color=? WHERE imageid=?`
-    connection.query(sqlQuery, [...data, imageid], (err:any, result:any)=>{
+    await connection.query(sqlQuery, [...data, imageid], (err:any, result:any)=>{
         if(err) return res.json({Status: false, Error: err.sqlMessage, error: "Query Error"})
         else return res.json({Satus: true, Messge: "Image updated successfully", result})
     })
